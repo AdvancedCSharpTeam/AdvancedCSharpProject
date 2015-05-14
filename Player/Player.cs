9be660c8e48409c2845bd601ec03bdc29a545a98
Peter Georgiev <shnogeorgiev@gmail.bg>
2015-05-14 20:51:13 +0300

    Initial Commit

---
+++ Player/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeamWork
+{
+    public class Player : IPlayer
+    {
+        public void MoveUp()
+        {
+            throw new System.NotImplementedException();
+        }
+        public void MoveDown()
+        {
+            throw new System.NotImplementedException();
+        }
+        public void MoveLeft()
+        {
+            throw new System.NotImplementedException();
+        }
+        public void MoveRight()
+        {
+            throw new System.NotImplementedException();
+        }
+
+    }
+}
