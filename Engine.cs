f353e4cc2ff11ee908f8fa17a2e5c6f685e86ea8
Peter Georgiev <shnogeorgiev@gmail.bg>
2015-05-14 23:06:54 +0300

    Engine Class Implemented

---
+++ Engine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeamWork
+{
+    public class Engine
+    {
+        //TODO: Implement Engine Class!
+        public Engine()
+        {
+            this.Start();
+        }
+
+        private void Start()
+        {
+            while (true)
+            {
+                this.TakeInput();
+            }
+        }
+        
+        private void TakeInput()
+        {
+            
+        }
+    }
+}
